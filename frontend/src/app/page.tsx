41c774ee831ad464300ba84782d01a9e4ab21118
Akash Gupta <acashgupta960@gmail.com>
2025-04-11 16:47:59 +0530

    My Initial Commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome</h1>
+      <p>This is a basic homepage for deployment.</p>
+    </div>
+  );
+}
